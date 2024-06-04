9f65c91df345fbd141d5da5bf2f7918dd9b2c9c7
heatherlyrobert <jelloshrike@gmail.com>
2024-06-04 05:48:13 -0700

    0.2n : big unit test improvement on database write and wave searches

--- ouroboros.h
+++ ouroboros.h
@@ -35,8 +35,8 @@
 /*········· ··········· ´·····························´········································*/
 #define     P_VERMAJOR  "0.-- preparing for production use"
 #define     P_VERMINOR  "0.2- adapt to new data requirements"
-#define     P_VERNUM    "0.2m"
-#define     P_VERTXT    "very basic database write unit tested"
+#define     P_VERNUM    "0.2n"
+#define     P_VERTXT    "big unit test improvement on database write and wave searches"
 /*········· ··········· ´·····························´········································*/
 #define     P_PRIORITY  "direct, simple, brief, vigorous, and lucid (h.w. fowler)"
 #define     P_PRINCIPAL "[grow a set] and build your wings on the way down (r. bradbury)"
