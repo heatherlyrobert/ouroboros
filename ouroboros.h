bf4a977a9b3d9dc7857bf33727b158627b5e8815
heatherlyrobert <jelloshrike@gmail.com>
2024-08-24 20:30:10 -0700

    0.4e : fixed all but last unit test scripts in graph section

--- ouroboros.h
+++ ouroboros.h
@@ -35,8 +35,8 @@
 /*········· ··········· ´·····························´········································*/
 #define     P_VERMAJOR  "0.-- preparing for production use"
 #define     P_VERMINOR  "0.4- handle predictable blocks/groups"
-#define     P_VERNUM    "0.4d"
-#define     P_VERTXT    "fixed all unit test scripts in incl section (after big dep update)"
+#define     P_VERNUM    "0.4e"
+#define     P_VERTXT    "fixed all but last unit test scripts in graph section"
 /*········· ··········· ´·····························´········································*/
 #define     P_PRIORITY  "direct, simple, brief, vigorous, and lucid (h.w. fowler)"
 #define     P_PRINCIPAL "[grow a set] and build your wings on the way down (r. bradbury)"
