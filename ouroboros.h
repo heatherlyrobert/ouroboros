240ddd08d2b4ea68a4fd029b512641a23c28f58b
heatherlyrobert <jelloshrike@gmail.com>
2024-08-29 04:54:57 -0700

    0.4h : udated/fixed data unit testing (little stuff)

--- ouroboros.h
+++ ouroboros.h
@@ -35,8 +35,8 @@
 /*········· ··········· ´·····························´········································*/
 #define     P_VERMAJOR  "0.-- preparing for production use"
 #define     P_VERMINOR  "0.4- handle predictable blocks/groups"
-#define     P_VERNUM    "0.4g"
-#define     P_VERTXT    "confirmed deps and wave unit testing"
+#define     P_VERNUM    "0.4h"
+#define     P_VERTXT    "udated/fixed data unit testing (little stuff)"
 /*········· ··········· ´·····························´········································*/
 #define     P_PRIORITY  "direct, simple, brief, vigorous, and lucid (h.w. fowler)"
 #define     P_PRINCIPAL "[grow a set] and build your wings on the way down (r. bradbury)"
